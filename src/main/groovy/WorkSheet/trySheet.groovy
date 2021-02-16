c36a3dd99badc96442b596c653f4be50664b2c9c
woodmawa <will.woodman@btinternet.com>
2021-02-16 18:27:44 +0000

    updated names

--- src/main/groovy/WorkSheet/trySheet.groovy
+++ src/main/groovy/WorkSheet/trySheet.groovy
@@ -5,6 +5,7 @@ import groovy.transform.Immutable
 import groovy.transform.MapConstructor
 
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentNavigableMap
 
 @MapConstructor
 @EqualsAndHashCode  //need this to ensure unique access via the cells map
@@ -82,10 +83,63 @@ class Cell {
         this.name = Optional<String>.of(name)
     }
 
+    void updateValue (final update) {
+        value = update
+    }
+
+    String getValueAsText () {
+        String BLANK_STRING = ""
+        if (value) {
+            value.toString()
+        } else {
+            BLANK_STRING
+        }
+    }
+
     String toString () {
         "cell @{$cellReference : $value}"
     }
 
+}
+
+class DatasetRow<Long, Cell> {
+    Optional<String> name = Optional.empty()
+    long rowNumber
+    ConcurrentHashMap rowCells = new ConcurrentHashMap<Long, Cell> ()
+
+    void setName (final String name) {
+        this.name = Optional.of (name)
+    }
+
+    void putCell (final Cell cell) {
+        assert cell
+        long row = cell.cellReference.x
+        long col = cell.cellReference.y
+
+        if (rowNumber == row) {
+            rowCells.put(col, cell)
+        }
+    }
+}
+
+class DatasetColumn<Long, Cell> {
+    Optional<String> name = Optional.empty()
+    long columnNumber
+    ConcurrentHashMap columnCells = new ConcurrentHashMap<Long, Cell> ()
+
+    void setName (final String name) {
+        this.name = Optional.of (name)
+    }
+
+    void putCell (final Cell cell) {
+        assert cell
+        long row = cell.cellReference.x
+        long col = cell.cellReference.y
+
+        if (columnNumber == col) {
+            columnCells.put(row, cell)
+        }
+    }
 }
 
 /**
@@ -95,17 +149,70 @@ class Cell {
 class Table {
 
     String name
-    ConcurrentHashMap namedRows = new ConcurrentHashMap<long, String>()
-    ConcurrentHashMap namedColumns = new ConcurrentHashMap<long, String>()
+    ConcurrentHashMap rows = new ConcurrentHashMap<long, DatasetRow>()
+    ConcurrentHashMap columns = new ConcurrentHashMap<long, DatasetColumn>()
 
     Closure error = {println "error didnt save cell ref - if was invalid $it.cellReference", "error invalid co-ordinate for cell"}
     Closure success = {println "all went well saved $it in table", "OK"}
 
+    //look at jigsaw table to help here
     ConcurrentHashMap cellsGrid = new ConcurrentHashMap<CellCoOrdinate, Cell>()
+
+    void setColumnName (long colNumber, final String name) {
+
+        DatasetColumn col = columns.get (colNumber)
+        col.setName(name)
+    }
+
+    void setRowName (long rowNumber, final String name) {
+
+        DatasetRow row = rows.get (rowNumber)
+        row.setName(name)
+    }
+
+    DatasetColumn getColumn (long colNumber) {
+
+        columns.get (colNumber)
+    }
+
+    DatasetRow getRow (long rowNumber) {
+
+        def row = rows.get (rowNumber)
+        row
+    }
+
+    private void addCellToRow (long rowNumber, Cell cell) {
+        DatasetRow row = rows.get(rowNumber)
+        if (row) {
+            row.putCell(cell)
+        } else {
+            row = new DatasetRow()
+            row.rowNumber = rowNumber
+            row.putCell(cell)
+            rows.put (rowNumber, row)
+
+        }
+    }
+
+    private void addCellToColumn (long colNumber, Cell cell) {
+        DatasetColumn col = columns.get(colNumber)
+        if (col) {
+            col.putCell(cell)
+        } else {
+            col = new DatasetColumn()
+            col.columnNumber = colNumber
+            col.putCell(cell)
+            columns.put(colNumber, col)
+
+        }
+    }
 
     void setCell (Cell cell) {
         if (cell.cellReference) {
-            cellsGrid.putIfAbsent(cell.cellReference, cell)
+            cellsGrid.put (cell.cellReference, cell)
+            addCellToRow (cell.cellReference.x, cell)
+            addCellToColumn (cell.cellReference.y, cell)
+
             cell
         }
         else {
@@ -115,23 +222,13 @@ class Table {
 
     void setCell (final List ref, def value) {
         CellCoOrdinate coOrdRef = new CellCoOrdinate(ref)
-        def cell = cellsGrid[coOrdRef]
-        if (cell) {
-            cell.value = value
-        } else {
-            cell = new Cell(cellReference:coOrdRef,value:value)
-            cellsGrid.putIfAbsent(coOrdRef, cell)
-        }
+        Cell cell = new Cell (cellReference: coOrdRef, value: value )
+        setCell (cell)
     }
 
     void setCell (CellCoOrdinate coOrdRef, value) {
-        def cell = cellsGrid[coOrdRef]
-        if (cell) {
-            cell.value = value
-        } else {
-            cell = new Cell(cellReference:coOrdRef,value:value)
-            cellsGrid.putIfAbsent(coOrdRef, cell)
-        }
+        Cell cell = new Cell (cellReference: coOrdRef, value: value )
+        setCell (cell)
     }
 
     Cell getCell (final List ref) {
@@ -152,10 +249,16 @@ CellCoOrdinate origin = new CellCoOrdinate (0,0)
 Table table = new Table(name:'myTab')
 table.setCell([0,0], 10)
 table.setCell([1,1], "cell 1:1")
+table.setCell([1,2], "cell 1:2")
 Cell c = table.getCell([0,0])
 Cell c2 = table.getCell(origin)
+
+table.setRowName(1,"row 1")
+
+DatasetRow row = table.getRow (1)
+println "got row " + row.name
 
 println table.name
 println c
 println c2
-println table.getCell([1,1]).value
+println table.getCell([1,1]).valueAsText
