a152e63e42353733f5305042436ff4346e32148a
woodmawa <will.woodman@btinternet.com>
2021-03-09 12:03:55 +0000

    think generic add for multi demensional matrix for col 1st of row first is now cracked

--- src/main/groovy/com/softwood/carray/fillArrays.groovy
+++ src/main/groovy/com/softwood/carray/fillArrays.groovy
@@ -88,7 +88,7 @@ def increment = {fillBy, limits, arrayValue, step ->
                     next[currentColumn++] = lower  //, reset this column, and post increment to the start value point for next column
                     //if we are processing the rows - handle the row column precedence first before handling columns 2...n
                     if (col == 1 && next[0] < limits[0]['upper']) {
-                        next[0] = next[0] + 1
+                        next[0] = next[0] + step
                         break
                     } else {
                         next[0] = limits[0]['lower']
@@ -154,4 +154,118 @@ for (i in 0..<40) {
 
 println fill
 
+def decrement = {fillBy, limits, arrayValue, step ->
+    ArrayList next = ArrayList.copyOf(arrayValue)
+    int startingColumn
+    int currentColumn
 
+    switch (fillBy) {
+        case ListFill.byColumnFirst:
+
+            startingColumn = 0
+            currentColumn = 0
+
+            def highLow = limits[startingColumn]
+            int upper = highLow['upper']
+            int lower = highLow['lower']
+
+            //start with currentValue as value in the column 0
+            def columnValue = arrayValue[currentColumn]
+
+            for (col in 0..<arrayValue.size()) {
+                if (columnValue > lower) {
+                    next[currentColumn] = columnValue - step
+                    break
+                }
+                else {
+                    next[currentColumn++] = upper
+                    columnValue =next[currentColumn] //get the start value point for next column
+                    highLow = limits[currentColumn]
+                    upper = highLow['upper']
+                    lower = highLow['lower']
+                }
+            }
+            break
+
+        case ListFill.byRowFirst:
+            startingColumn = 1
+            currentColumn = 1
+
+            def highLow = limits[startingColumn]
+            int upper = highLow['upper']
+            int lower = highLow['lower']
+
+            //start with currentValue as value in the column 0
+            def columnValue = arrayValue[currentColumn]
+
+            for (col in 1..<arrayValue.size()) {
+                if (columnValue > lower) {
+                    next[currentColumn] = columnValue - step
+                    break
+                }
+                else {
+                    next[currentColumn++] = upper  //, reset this column, and post increment to the start value point for next column
+                    //if we are processing the rows - handle the row column precedence first before handling columns 2...n
+                    if (col == 1 && next[0] > limits[0]['lower']) {
+                        next[0] = next[0] - step
+                        break
+                    } else {
+                        next[0] = limits[0]['lower']
+                    }
+                    columnValue =next[currentColumn] //get the start value point for next column
+                    highLow = limits[currentColumn]
+                    upper = highLow['upper']
+                    lower = highLow['lower']
+                }
+            }
+            break
+
+        default:
+            next = null
+    }
+
+    next
+}
+
+fill = []
+fill << [1,1,1]
+ans = [1,1,1]
+
+for (i in 0..<40) {
+    ans = decrement (ListFill.byColumnFirst, arrayIndexLimits, ans, 1)
+    //reached end of this
+
+
+    fill << ans
+
+    if (ans == [0,0,0]) {
+        println "decrement reached bottom value column fill first  $ans"
+        break
+    }
+
+    ans
+}
+
+println fill
+
+fill = []
+fill << [1,1,1]
+ans = [1,1,1]
+
+for (i in 0..<40) {
+    ans = decrement (ListFill.byRowFirst, arrayIndexLimits, ans, 1)
+    //reached end of this
+
+
+    fill << ans
+
+    if (ans == [0,0,0]) {
+        println "decrement reached bottom value row fill first  $ans"
+        break
+    }
+
+    ans
+}
+
+
+println fill
