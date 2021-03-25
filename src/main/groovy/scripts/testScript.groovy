21f88db6cec81bf022b9b02181ab0590056e1027
woodmawa <will.woodman@btinternet.com>
2021-03-25 16:53:53 +0000

    added test json parse script test

--- src/main/groovy/scripts/testScript.groovy
+++ src/main/groovy/scripts/testScript.groovy
@@ -2,12 +2,16 @@ package scripts
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.stream.Collectors
 import java.util.stream.Stream
 import java.util.concurrent.TimeUnit
 
 import static java.util.stream.Collectors.*
 
 println "hello"
+
+def res = Stream.of("hello").map(s -> s::concat(" Will")).collect(Collectors.toList())
+println res
 
 def start = System.nanoTime()
 new File("D:\\OneDrive\\wills-sync\\Tech M\\VF UK OpCo Systems and programmes\\TM for VF design council\\UK Data Lake and monitisation\\companies data\\BasicCompanyDataAsOneFile-2019-09-01.csv".toString()).withInputStream { inps ->
